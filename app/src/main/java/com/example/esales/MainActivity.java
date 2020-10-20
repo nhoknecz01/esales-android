dc18246906e9ce63202332e812e2ca83dae5dd80
nhoknecz01 <nguyenhuutri1007@gmail.com>
2020-10-21 01:19:25 +0700

    Commit API Login
    đối ứng api login
    
    Co-Authored-By: tri <40267774+tri@users.noreply.github.com>

--- app/src/main/java/com/example/esales/MainActivity.java
+++ app/src/main/java/com/example/esales/MainActivity.java
@@ -2,13 +2,122 @@ package com.example.esales;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.esales.constants.URLs;
+import com.example.esales.volley.VolleyRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    RequestQueue requestQueue;
+    TextView mTextViewResult;
+    EditText txtUsername, txtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(this);
+
+        mTextViewResult = (TextView) findViewById(R.id.idError);
+        txtUsername = (EditText)findViewById(R.id.txtUsername);
+        txtPassword = (EditText)findViewById(R.id.txtPassword);
+
+
+        findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //if user pressed on button login
+                //here we will login the user to server
+                login();
+            }
+        });
     }
+
+    private void login() {
+        final String username = txtUsername.getText().toString();
+        final String password = txtPassword.getText().toString();
+
+        if(TextUtils.isEmpty(username)) {
+            txtUsername.setError("Please enter username");
+            txtUsername.requestFocus();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)) {
+            txtPassword.setError("Please enter password");
+            txtPassword.requestFocus();
+            return;
+        }
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URLs.URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            //converting response to json object
+                            JSONObject obj = new JSONObject(response);
+
+                            //if no error in response
+                            if (!obj.getBoolean("error")) {
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+
+                                //getting the user from the response
+                                JSONObject tokenJson = obj.getJSONObject("token");
+
+                                //starting the profile activity
+                                finish();
+                                mTextViewResult.append("Thành công");
+//                                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                            } else {
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        VolleyRequest.getInstance(this).addToRequestQueue(stringRequest);
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/esales/constants/URLs.java
@@ -0,0 +1,8 @@
+package com.example.esales.constants;
+
+public class URLs {
+    private static final String ROOT_URL = " http://localhost:8090/api/v1/auth/";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN = ROOT_URL + "signin";
+}
